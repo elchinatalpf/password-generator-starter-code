671010f47cb6fda558a9a8f46f98c00eff78c913
Javier <elchinatalpf@github.com>
2023-07-01 19:21:47 -0500

    prompts done

--- Develop/script.js
+++ Develop/script.js
@@ -1,69 +1,86 @@
 // Assignment code here
-// generateBtn is the  Buttom's container that generate password
-// the function writePassword is being call by the addEventListener generateBtn that contains
-// the function generatePassword. So I need to create the function generatePassword
-// the return inside the generatePassword function shows that the 
+
+// maybe to use
+// charactersALL = charactersALL.concat(lowerCase, upperCase, numbers, specialCharacters); 
+// for (var i = 0; i < passwordLength; i++) {
+//   var randomIndex = Math.floor(Math.random() * charactersALL.length);
+//   password += charactersALL[randomIndex];
+// }
 
 
+var charactersALL = [];
+var passwordLength = 8;
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",
+"p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var numbers = ["1","2","3","4","5","6","7","8","9","0"];
+var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",",
+"-", ".", ":", ";", "<", "=", ">", "?", "@", "[", "/", "]", "^", "_", "`",
+"{", "|", "}", "~"]; // 30 length, 29 values starting from 0
+
+
+function userPrompts () {
+  var charactersALL = [];
+  passwordLength = parseInt(prompt("How many characters would you like to use between 8 and 128? "));
+  
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("The password's length has to be between 8 and 128 characters");
+    return false;
+  }
+  if (confirm("Would you like to use lower case letters?")) {
+    charactersALL = charactersALL.concat(lowerCase);
+  }
+  if (confirm("Would you like to use upper case letters?")) {
+    charactersALL = charactersALL.concat(upperCase);
+  }
+  if (confirm("Would you like to use numbers?")) {
+    charactersALL = charactersALL.concat(numbers);
+  }
+  if (confirm("Would you like to use special characters?")) {
+    charactersALL = charactersALL.concat(specialChar);
+  }
+  return true;
+}
 
 
 function generatePassword() {
-  console.log("this is fun");
-  var userChoice = {
-    
-  } 
+  var correctPrompts = userPrompts();
+  // console.log("this is fun");
+  var password = "";
+  for(var i = 0; i < passwordLength; i++) {
+    var randomIndex = Math.floor(Math.random() * charactersALL.length);
+    password = password * charactersALL[randomIndex];
 
-
-
-
-  return "Generated Password"; //here goes the var that contains the password value generated
+  }
+  
+  return "Your password is: " + password; //here goes the random generated password
+  console.log(password);
 }
 
+
+
+// this was the code provided for the homework
 generatePassword();
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-console.log(generateBtn);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts =  userPrompts();
+  
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-  
+  if (correctPrompts) {
+    var password = generatePassword();
+    
+  } else {
+    passwordText.value = password;
+  }
+
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-
-
-// ## User Story
-
-// ```
-// AS AN employee with access to sensitive data
-// I WANT to randomly generate a password that meets certain criteria
-// SO THAT I can create a strong password that provides greater security
-// ```
-
-// ## Acceptance Criteria
-
-// ```
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-// THEN I am presented with a series of prompts for password criteria
-// WHEN prompted for password criteria
-// THEN I select which criteria to include in the password
-// WHEN prompted for the length of the password
-// THEN I choose a length of at least 8 characters and no more than 128 characters
-// WHEN asked for character types to include in the password
-// THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-// WHEN I answer each prompt
-// THEN my input should be validated and at least one character type should be selected
-// WHEN all prompts are answered
-// THEN a password is generated that matches the selected criteria
-// WHEN the password is generated
-// THEN the password is either displayed in an alert or written to the page
-// ```
